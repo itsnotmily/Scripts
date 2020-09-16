c1e542e5c5f6572f524931b34cbce6fafa25d4ca
Milan <milz1337@gmail.com>
2020-09-16 17:24:47 +0100

    Delete instagramPic.js

--- Scripts/instagramPic.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-console.log(event);
-window.open(event.srcElement.previousSibling.firstChild.srcset.split(",")[2].split(" ")[0]);
-}
-document.body.addEventListener("click", myFunction);
